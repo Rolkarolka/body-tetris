c78226a372f7442b73a7863c80bd9810df6d647d
Karolina Romanowska <k.romanowska@stud.elka.pw.edu.pl>
2023-05-13 16:13:41 +0200

    add buttons to change screen

--- video_player.py
+++ video_player.py
@@ -1,68 +1,55 @@
-# PyQt5 Video player
-#!/usr/bin/env python
 import os.path
 
-from PyQt5.QtCore import QDir, Qt, QUrl
+from PyQt5.QtCore import  QUrl
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 from PyQt5.QtMultimediaWidgets import QVideoWidget
-from PyQt5.QtWidgets import (QApplication, QFileDialog, QHBoxLayout, QLabel,
-        QPushButton, QSizePolicy, QSlider, QStyle, QVBoxLayout, QWidget)
-from PyQt5.QtWidgets import QMainWindow,QWidget, QPushButton, QAction, QDialog
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QVBoxLayout, QDialog, QPushButton, QHBoxLayout
 import sys
 
 class VideoWindow(QDialog):
 
-    def __init__(self):
+    def __init__(self, filename="start.avi"):
+        self.filename = filename
         super(VideoWindow, self).__init__()
 
-        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+        self.player = QMediaPlayer(None, QMediaPlayer.VideoSurface)
 
-        videoWidget = QVideoWidget()
+        self.video = QVideoWidget()
 
-        self.moveButton = QPushButton()
-        self.moveButton.setText("Play game")
-        self.moveButton.clicked.connect(self.hello)
-        controlLayout = QHBoxLayout()
-        controlLayout.addWidget(self.moveButton)
-        controlLayout.addWidget(videoWidget)
-        
-        # Create exit action
-        exitAction = QAction(QIcon('exit.png'), '&Exit', self)
-        exitAction.setShortcut('Ctrl+Q')
-        exitAction.setStatusTip('Exit application')
-        exitAction.triggered.connect(self.exitCall)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.video)
+        self.setLayout(self.layout)
+
+        game_button = QPushButton()
+        game_button.setText("Play Game")
+        game_button.clicked.connect(self.go_to_tetris)
+        exercises_button = QPushButton()
+        exercises_button.setText("Play Game")
+        exercises_button.clicked.connect(self.play_exercises)
+        self.buttons_layout = QHBoxLayout()
+        self.buttons_layout.addWidget(game_button)
+        self.buttons_layout.addWidget(exercises_button)
 
 
-        # Create a widget for window contents
-        wid = QWidget(self)
-        # self.setCentralWidget(wid)
+        self.player.setVideoOutput(self.video)
+        self.player.mediaStatusChanged.connect(self.statusChanged)
+        self.openFile(self.filename)
+        self.player.play()
 
-        layout = QVBoxLayout()
-        # layout.addWidget(videoWidget)
-        layout.addLayout(controlLayout)
+    def statusChanged(self, status):
+        if status == QMediaPlayer.EndOfMedia:
+            self.layout.addLayout(self.buttons_layout)
 
-        # Set widget to contain window contents
-        wid.setLayout(layout)
+    def go_to_tetris(self):
+        print("Tetris time")
 
-        self.mediaPlayer.setVideoOutput(videoWidget)
-
-        self.openFile("start.avi")
-        self.play()
 
     def openFile(self, filename):
-        self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(os.path.join(".", "video", filename))))
+        self.player.setMedia(QMediaContent(QUrl.fromLocalFile(os.path.join(".", "video", filename))))
 
     def exitCall(self):
         sys.exit(app.exec_())
 
-    def play(self):
-        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
-            self.mediaPlayer.pause()
-        else:
-            self.mediaPlayer.play()
-    def hello(self):
-        print("hello")
     def handleError(self):
         print("Error: " + self.mediaPlayer.errorString())
 

7e7759d621933a83e521345dbf9a7e6e0fe21cd6
Karolina Romanowska <k.romanowska@stud.elka.pw.edu.pl>
2023-05-14 15:05:36 +0200

    connect to camera

--- tetris_game.py
+++ tetris_game.py
@@ -1,7 +1,9 @@
-
-from PyQt5.QtWidgets import QDesktopWidget, QHBoxLayout
-from PyQt5.QtCore import QBasicTimer
-from PyQt5.QtGui import QPainter, QColor
+import cv2
+import numpy as np
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QDesktopWidget, QHBoxLayout, QLabel
+from PyQt5.QtCore import QBasicTimer, pyqtSlot, Qt
+from PyQt5.QtGui import QPainter, QColor, QPixmap
 from PyQt5.QtWidgets import QDialog, QFrame
 
 from tetris_model import BOARD_DATA, Shape
@@ -20,23 +22,33 @@ class Tetris(QDialog):
         self.gridSize = 22
         self.speed = 600
         self.pose_speed = 100
+        self.camera_width = 320
+        self.camera_height = 240
 
         self.timer = QBasicTimer()
-        self.pose_timer = QBasicTimer()
         # self.setFocusPolicy(Qt.StrongFocus)
 
         board_layout = QHBoxLayout()
 
+
         self.tboard = Board(self, self.gridSize)
         self.sidePanel = SidePanel(self, self.gridSize)
+        self.image_label = QLabel(self)
+        self.image_label.resize(self.camera_width, self.camera_height)
+
         board_layout.addWidget(self.tboard)
         board_layout.addWidget(self.sidePanel)
+        board_layout.addWidget(self.image_label)
+
+        self.thread = PoseExtractor()
+        self.thread.change_pixmap_signal.connect(self.update_image)
+        self.thread.make_move_signal.connect(self.get_move)
+        self.thread.start()
 
         self.setLayout(board_layout)
         self.start()
         self.center()
 
-        self.poseExtractor = PoseExtractor()
 
     def center(self):
         screen = QDesktopWidget().screenGeometry()
@@ -54,7 +66,6 @@ class Tetris(QDialog):
 
         BOARD_DATA.createNewPiece()
         self.timer.start(self.speed, self)
-        self.pose_timer.start(self.pose_speed, self)
 
     def pause(self):
         if not self.isStarted:
@@ -66,7 +77,6 @@ class Tetris(QDialog):
             self.timer.stop()
         else:
             self.timer.start(self.speed, self)
-            self.pose_timer.start(self.pose_timer, self)
 
         self.update_window()
 
@@ -75,16 +85,29 @@ class Tetris(QDialog):
         self.sidePanel.update_data()
         self.update()
 
-    def timerEvent(self, event):
-        if event.timerId() == self.pose_timer.timerId():
-            self.get_move()
+    def closeEvent(self, event):
+        self.thread.stop()
+        event.accept()
 
+    @pyqtSlot(np.ndarray)
+    def update_image(self, cv_img):
+        qt_img = self.convert_cv_qt(cv_img)
+        self.image_label.setPixmap(qt_img)
+
+    def convert_cv_qt(self, cv_img):
+        rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+        h, w, ch = rgb_image.shape
+        bytes_per_line = ch * w
+        convert_to_qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
+        p = convert_to_qt_format.scaled(self.camera_width, self.camera_height, Qt.KeepAspectRatio)
+        return QPixmap.fromImage(p)
+
+    def timerEvent(self, event):
         if event.timerId() == self.timer.timerId():
             lines, result = BOARD_DATA.moveDown()
             if not result:
                 self.pause()
                 self.return_screen()
-                # self.__init__(self.get_pose)
             self.tboard.score += lines
             if self.lastShape != BOARD_DATA.currentShape:
                  self.lastShape = BOARD_DATA.currentShape
@@ -92,14 +115,11 @@ class Tetris(QDialog):
         else:
             super(Tetris, self).timerEvent(event)
 
-    def get_move(self):
+    @pyqtSlot(Move)
+    def get_move(self, move):
         if not self.isStarted or BOARD_DATA.currentShape == Shape.shapeNone:
             return
 
-        # move = self.get_pose()
-        move = self.poseExtractor.get_pose()
-        if move is not None:
-            print(move) # TODO
         if self.isPaused:
             return
         elif move == Move.LEFT:
